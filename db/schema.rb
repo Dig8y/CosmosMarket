6895ca5a8b5fbddb9fbe2a60edebe8f1efcc7e42
Digby Hebb <digby.hebb@gmail.com>
2023-11-20 15:17:54 +0000

    land and booking models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_151142) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_151540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "land_id", null: false
+    t.float "price"
+    t.date "booking_start_date"
+    t.date "booking_end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["land_id"], name: "index_bookings_on_land_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "lands", force: :cascade do |t|
+    t.integer "area"
+    t.bigint "user_id", null: false
+    t.string "local_attractions"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_151142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "lands"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "lands", "users"
 end
