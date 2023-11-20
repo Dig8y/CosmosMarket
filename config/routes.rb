acca6b545155dcca11e50c42fd959cf0e23a4bb1
shakir mohamed <madsayain22@gmail.com>
2023-11-20 16:43:20 +0000

    Added lands controller and show.html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'lands/index'
   root to: "pages#home"
   devise_for :users
 
   resources :bookings
+  resources :lands
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
