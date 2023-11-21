6706ef2513ed6db933ae463f4af3922a7c374f7b
will caplan <willcaplan9@gmail.com>
2023-11-21 14:58:29 +0000

    adding dashboard and seed booking

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,5 +1,7 @@
 class Land < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
   validates :area, presence: true
   validates :area, numericality: { only_integer: true }
   validates :address, presence: true
