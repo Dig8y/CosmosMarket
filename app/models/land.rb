cff4c2ca2decf79b30d509624ed4231889ce99d3
will caplan <willcaplan9@gmail.com>
2023-11-22 11:39:30 +0000

    added image upload to new page

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,7 +1,7 @@
 class Land < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many_attached :images
+  has_many_attached :photos
 
   validates :area, presence: true
   validates :area, numericality: { only_integer: true }
