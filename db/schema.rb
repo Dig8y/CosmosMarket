ac4936384812dede591c51b7948fdb042c83b4a8
Digby Hebb <digby.hebb@gmail.com>
2023-11-23 10:51:24 +0000

    fixed seed file and then dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_112820) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_101318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_112820) do
     t.datetime "updated_at", null: false
     t.float "price"
     t.string "images", default: [], array: true
+    t.text "description"
+    t.string "planet_name"
     t.index ["user_id"], name: "index_lands_on_user_id"
   end
 
